f47ce9a1aad4faaf43dafd655e6ad536f7e3f50f
Eric Jordan <ewjordan@gmail.com>
2008-05-01 23:55:03 +0000

    Demo changes

--- src/org/jbox2d/testbed/tests/BugTest.java
+++ src/org/jbox2d/testbed/tests/BugTest.java
@@ -32,6 +32,7 @@ import org.jbox2d.dynamics.joints.DistanceJointDef;
 import org.jbox2d.dynamics.joints.RevoluteJointDef;
 import org.jbox2d.testbed.AbstractExample;
 import org.jbox2d.testbed.TestbedMain;
+import org.jbox2d.util.blob.*;
 
 public class BugTest extends AbstractExample {
 	private boolean firstTime;
@@ -59,61 +60,11 @@ public class BugTest extends AbstractExample {
 			ground = m_world.createBody(bd);
 			ground.createShape(sd);
 		}
+		
+		BlobMaker.createBlob(new SquareLatticeStructure(),
+							 new CircularBlobContainer(new Vec2(0.0f,10.0f),5.0f),
+							 m_world,0.5f, 0.5f, 0.15f, 0.15f);
 
-		{
-			PolygonDef sd = new PolygonDef();
-			sd.setAsBox(0.65f, 0.125f);
-			sd.density = 20.0f;
-			sd.friction = 0.2f;
-
-			DistanceJointDef jd = new DistanceJointDef();
-			int numPlanks = 30;
-
-			Body prevBody = ground;
-			for (int i = 0; i < numPlanks; ++i) {
-				BodyDef bd = new BodyDef();
-				bd.position.set(-14.5f + 1.0f * i, 10.0f);
-				Body body = m_world.createBody(bd);
-				body.createShape(sd);
-				body.setMassFromShapes();
-
-				Vec2 anchor = new Vec2(-15.0f + 1.0f * i, 10.0f);
-				Vec2 anchor2 = new Vec2(-14.8f + 1.0f * i, 10.0f);
-				jd.dampingRatio = 0.9f;
-				jd.frequencyHz = 50.0f;
-				jd.initialize(prevBody, body, anchor, anchor2);
-				m_world.createJoint(jd);
-
-				prevBody = body;
-			}
-			Vec2 anchor2 = new Vec2(-15.0f + 1.0f * numPlanks, 10.0f);
-			Vec2 anchor = new Vec2(-14.8f + 1.0f * numPlanks, 10.0f);
-			jd.initialize(prevBody, ground, anchor, anchor2);
-			m_world.createJoint(jd);
-			
-			PolygonDef pd2 = new PolygonDef();
-			pd2.setAsBox(1.0f,1.0f);
-			pd2.density = 5.0f;
-			pd2.friction = 0.2f;
-			pd2.restitution = 0.1f;
-			BodyDef bd2 = new BodyDef();
-			bd2.position.set(0.0f, 10.0f);
-			Body body2 = m_world.createBody(bd2);
-			body2.createShape(pd2);
-			body2.setMassFromShapes();
-			
-			CircleDef cd = new CircleDef();
-			cd.radius = 0.9f;
-			cd.density = 5.0f;
-			cd.friction = 0.2f;
-			BodyDef bd3 = new BodyDef();
-			bd3.position.set(0.0f, 12.0f);
-			Body body3 = m_world.createBody(bd3);
-			body3.createShape(cd);
-			cd.localPosition.set(0.0f,1.0f);
-			body3.createShape(cd);
-			body3.setMassFromShapes();
-		}
 
 	}
 
