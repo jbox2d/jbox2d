5f3af673ac4e011c19e082f4d9e6fc831f42e8fd
Daniel Murphy <danielmurphy161@gmail.com>
2009-09-08 15:23:55 +0000

    finished updating collision library in update project to the latest box2d revision

--- trunk/updating/src/org/jbox2d/structs/collision/broadphase/Pair.java
+++ trunk/updating/src/org/jbox2d/structs/collision/broadphase/Pair.java
@@ -2,25 +2,34 @@ package org.jbox2d.structs.collision.broadphase;
 
 import org.jbox2d.collision.broadphase.DynamicTreeNode;
 
+/**
+ * Java note: at the "creation" (when retrieved from pool) of each node, a random key is given to
+ * that node, and that's what we sort from.
+ */
 public class Pair implements Comparable<Pair>{
 	public DynamicTreeNode proxyA;
 	public DynamicTreeNode proxyB;
+	
+	
+	@Override
+	public int compareTo(Pair pair2) {
+		if (this.proxyA.key < pair2.proxyA.key){
+			return -1;
+		}
 
-	
-	public final int compareTo( Pair pair){
-		return compareCode() < pair.compareCode() ? -1 : 1;
-	}
-	
-	private final int compareCode(){
-		int hash = 1;
-		hash = 19*hash + (int)proxyA.aabb.lowerBound.x;
-		hash = 19*hash + (int)proxyA.aabb.lowerBound.y;
-		hash = 19*hash + (int)proxyA.aabb.upperBound.x;
-		hash = 19*hash + (int)proxyA.aabb.upperBound.y;
-		hash = 19*hash + (int)proxyB.aabb.lowerBound.x;
-		hash = 19*hash + (int)proxyB.aabb.lowerBound.y;
-		hash = 19*hash + (int)proxyB.aabb.upperBound.x;
-		hash = 19*hash + (int)proxyB.aabb.upperBound.y;
-		return hash;
+		if (this.proxyA.key == pair2.proxyA.key){
+			
+			if(proxyB.key < pair2.proxyB.key){
+				return -1;
+			}else{
+				if(proxyB.key == pair2.proxyB.key){
+					return 0;
+				}else{
+					return 1;
+				}
+			}
+		}
+
+		return 1;
 	}
 }
