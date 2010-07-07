562c7e047b30d9be25ca4774280e56b0ffa7f120
Daniel Murphy <danielmurphy161@gmail.com>
2010-07-07 07:58:53 +0000

    updated contact package

--- updating/src/main/java/org/jbox2d/dynamics/contacts/PolygonAndCircleContact.java
+++ updating/src/main/java/org/jbox2d/dynamics/contacts/PolygonAndCircleContact.java
@@ -2,41 +2,28 @@ package org.jbox2d.dynamics.contacts;
 
 import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.collision.shapes.PolygonShape;
-import org.jbox2d.dynamics.Body;
+import org.jbox2d.common.Transform;
 import org.jbox2d.dynamics.Fixture;
 import org.jbox2d.pooling.SingletonPool;
+import org.jbox2d.structs.collision.Manifold;
 import org.jbox2d.structs.collision.shapes.ShapeType;
-import org.jbox2d.structs.dynamics.contacts.ContactCreator;
 
-public class PolygonAndCircleContact extends Contact implements ContactCreator {
+// updated to rev 100
+
+public class PolygonAndCircleContact extends Contact {
 	
 	public PolygonAndCircleContact(){}
 	
 	public PolygonAndCircleContact(Fixture fixtureA, Fixture fixtureB) {
 		super(fixtureA, fixtureB);
-		assert(m_fixtureA.getType() == org.jbox2d.structs.collision.shapes.POLYGON_SHAPE);
-		assert(m_fixtureB.getType() == org.jbox2d.structs.collision.shapes.CIRCLE_SHAPE);
+		assert(m_fixtureA.getType() == ShapeType.POLYGON);
+		assert(m_fixtureB.getType() == ShapeType.CIRCLE);
 	}
 
 	@Override
-	protected void evaluate() {
-		Body bodyA = m_fixtureA.getBody();
-		Body bodyB = m_fixtureB.getBody();
-		
+	public void evaluate(Manifold manifold, Transform xfA, Transform xfB) {
 		SingletonPool.getCollision().collidePolygonAndCircle(m_manifold,
-				(PolygonShape)m_fixtureA.getShape(), bodyA.getTransform(),
-				(CircleShape)m_fixtureB.getShape(), bodyB.getTransform());
+				(PolygonShape)m_fixtureA.getShape(), xfA,
+				(CircleShape)m_fixtureB.getShape(), xfB);
 	}
-
-	@Override
-	public Contact contactCreateFcn(Fixture fixtureA, Fixture fixtureB) {
-		// TODO djm: pool these
-		return new PolygonAndCircleContact(fixtureA, fixtureB);
-	}
-
-	@Override
-	public void contactDestroyFcn(Contact contact) {
-		// TODO djm: pooling
-	}
-
 }
