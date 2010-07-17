c773781b0a6ac11cd74780f53e33a9c691bf2990
Daniel Murphy <danielmurphy161@gmail.com>
2010-07-17 08:32:55 +0000

    * removed unecessary pooling * added int[] pool * implemented an abstract method for convenience in DebugDraw

--- updating/jbox2d-library/src/main/java/org/jbox2d/callbacks/DebugDraw.java
+++ updating/jbox2d-library/src/main/java/org/jbox2d/callbacks/DebugDraw.java
@@ -49,12 +49,27 @@ public abstract class DebugDraw {
 	}
 
 	/**
-	 * Draw a closed polygon provided in CCW order.
+	 * Draw a closed polygon provided in CCW order.  This implementation
+	 * uses {@link #drawSegment(Vec2, Vec2, Color3f)} to draw each side of the
+	 * polygon.
 	 * @param vertices
 	 * @param vertexCount
 	 * @param color
 	 */
-	public abstract void drawPolygon(Vec2[] vertices, int vertexCount, Color3f color);
+	public void drawPolygon(Vec2[] vertices, int vertexCount, Color3f color){
+		if(vertexCount == 1){
+			drawSegment(vertices[0], vertices[0], color);
+			return;
+		}
+		
+		for(int i=0; i<vertexCount-1; i+=2){
+			drawSegment(vertices[i], vertices[i+1], color);
+		}
+		
+		if(vertexCount > 2){
+			drawSegment(vertices[vertexCount-1], vertices[0], color);
+		}
+	}
 
 	/**
 	 * Draw a solid closed polygon provided in CCW order.
