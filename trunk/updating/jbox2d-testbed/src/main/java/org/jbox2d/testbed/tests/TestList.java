a017b0a68af32945ab55bd39c99302fac5b7587a
Daniel Murphy <danielmurphy161@gmail.com>
2010-08-12 06:08:58 +0000

    added domino test

--- trunk/updating/jbox2d-testbed/src/main/java/org/jbox2d/testbed/tests/TestList.java
+++ trunk/updating/jbox2d-testbed/src/main/java/org/jbox2d/testbed/tests/TestList.java
@@ -1,13 +1,11 @@
 /**
  * Created at 5:34:33 PM Jul 17, 2010
  */
-package org.jbox2d.testbed.framework;
+package org.jbox2d.testbed.tests;
 
 import java.util.ArrayList;
 
-import org.jbox2d.testbed.tests.OneSidedTest;
-import org.jbox2d.testbed.tests.PyramidTest;
-import org.jbox2d.testbed.tests.VaryingFriction;
+import org.jbox2d.testbed.framework.TestbedTest;
 
 /**
  * @author Daniel Murphy
@@ -15,7 +13,8 @@ import org.jbox2d.testbed.tests.VaryingFriction;
 public class TestList {
 	public static final ArrayList<TestbedTest> tests = new ArrayList<TestbedTest>();
 	static{
-		tests.add(new VaryingFriction());
+		tests.add(new VaryingFrictionTest());
+		tests.add(new DominoTest());
 		tests.add(new PyramidTest());
 		tests.add(new OneSidedTest());
 	}
