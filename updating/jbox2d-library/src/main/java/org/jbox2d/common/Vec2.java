b056dd0f039d11d1806f87d27a088134faf19347
Daniel Murphy <danielmurphy161@gmail.com>
2010-08-15 04:24:54 +0000

    added more stats

--- updating/jbox2d-library/src/main/java/org/jbox2d/common/Vec2.java
+++ updating/jbox2d-library/src/main/java/org/jbox2d/common/Vec2.java
@@ -30,7 +30,7 @@ package org.jbox2d.common;
  */
 public class Vec2 {
 	/** Should we count Vec2 creations? */
-	static public boolean watchCreations = true;
+	static public boolean watchCreations = false;
 	/**
 	 * Running count of Vec2 creations.  Must be zeroed out
 	 * manually (perhaps at start of time step).  Incremented
