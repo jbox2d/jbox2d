02bb2167ae45f0410f6435f24262fe40a51bd960
Daniel Murphy <danielmurphy161@gmail.com>
2011-01-13 03:14:46 +0000

    distance joint and constant volume joint! mouse joint is still a bit weird

--- updating/jbox2d-library/src/main/java/org/jbox2d/structs/dynamics/joints/JointType.java
+++ updating/jbox2d-library/src/main/java/org/jbox2d/structs/dynamics/joints/JointType.java
@@ -3,5 +3,5 @@ package org.jbox2d.structs.dynamics.joints;
 //updated to rev 100 - ec
 public enum JointType {
 	UNKNOWN, REVOLUTE, PRISMATIC, DISTANCE, PULLEY,
-	MOUSE, GEAR, LINE, WELD, FRICTION
+	MOUSE, GEAR, LINE, WELD, FRICTION, CONSTANT_VOLUME
 }
