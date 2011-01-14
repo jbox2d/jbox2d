625789d8ce00ccd5735b24f4cabe27b0f4c85576
Daniel Murphy <danielmurphy161@gmail.com>
2011-01-14 21:24:47 +0000

    edge shapes test, more bugs

---
+++ updating/jbox2d-testbed/conv.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/sed -f
+s/b2//g
+s/->/./g
+s/Body\*/Body/g
+s/(Vec2/(new Vec2/g
+s/, Vec2/, new Vec2/g
+s/\.Set/.set/g
+s/\.Get/.get/g
+s/\.Cre/.cre/g
+s/\.Ini/.ini/g
+s/\.Des/.des/g
+s/NULL/null/g
+s/const/final/g
+s/float32/float/g
+s/bool /boolean /g
+s/int32/int/g
+s/&bd/bd/g
+s/&fd/fd/g
+s/BodyDef bd;/BodyDef bd = new BodyDef();/g
+s/PolygonShape shape;/PolygonShape shape = new PolygonShape();/g
+s/CircleShape shape;/CircleShape shape = new CircleShape();/g
+s/FixtureDef fd;/FixtureDef fd = new FixtureDef();/g
+s/m_world/world/g
+s/_pi/MathUtils.PI/g
+s/RandomFloat/MathUtils.randomFloat/g
+s/_dynamicBody/BodyType.DYNAMIC/g
+s/Abs(/MathUtils.abs(/g
+s/ sin(/ MathUtils.sin(/g
+s/ sinf(/ MathUtils.sin(/g
+s/ cos(/ MathUtils.cos(/g
+s/ cosf(/ MathUtils.cos(/g
+s/m_debugDraw/debugDraw/g
+s/Color(/new Color3f(/g
+s/\.Draw/.draw/g
