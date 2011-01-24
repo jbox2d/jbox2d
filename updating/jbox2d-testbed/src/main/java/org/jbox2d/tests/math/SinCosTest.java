8082d6fa7fb46cbdfc1878774d8b9c9c0cceff8b
Daniel Murphy <danielmurphy161@gmail.com>
2011-01-24 03:06:27 +0000

    put license in testbed files

--- updating/jbox2d-testbed/src/main/java/org/jbox2d/tests/math/SinCosTest.java
+++ updating/jbox2d-testbed/src/main/java/org/jbox2d/tests/math/SinCosTest.java
@@ -1,3 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2011, Daniel Murphy
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL DANIEL MURPHY BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
 package org.jbox2d.tests.math;
 
 import org.jbox2d.common.MathUtils;
@@ -228,7 +254,7 @@ public class SinCosTest {
 		long startTime, endTime;
 		long totalTime = 0;
 		float i,j;
-		float k;
+		float k = 0;
 		
 		final float jstep = MathUtils.TWOPI/numIterations;
 		
@@ -236,7 +262,7 @@ public class SinCosTest {
 			
 			startTime = System.nanoTime();
 			for(j=0; j<MathUtils.TWOPI; j+=jstep){
-				k = table.sin(j);
+				k += table.sin(j);
 			}
 			endTime = System.nanoTime();
 			totalTime += endTime - startTime;
@@ -249,7 +275,7 @@ public class SinCosTest {
 		long startTime, endTime;
 		long totalTime = 0;
 		float i,j;
-		float k;
+		float k = 0;
 		
 		final float jstep = MathUtils.TWOPI/numIterations;
 		
@@ -257,7 +283,7 @@ public class SinCosTest {
 			
 			startTime = System.nanoTime();
 			for(j=0; j<MathUtils.TWOPI; j+=jstep){
-				k = (float) StrictMath.sin(j);
+				k += (float) StrictMath.sin(j);
 			}
 			endTime = System.nanoTime();
 			totalTime += endTime - startTime;
