e9a6a368305f94d9be2d53080c864579c480311a
Daniel Murphy <danielmurphy161@gmail.com>
2011-07-21 08:27:31 +0000

    working a bit with the serialization framework

---
+++ trunk/jbox2d-serialization/src/main/java/org/jbox2d/serialization/UnsupportedListener.java
@@ -0,0 +1,17 @@
+package org.jbox2d.serialization;
+
+/**
+ * Used to hear when an object is unsupported by the serializer or the deserializer.
+ * This is so the de/serializer can keep going without throwing an exception and
+ * stopping the entire thing
+ * @author Daniel Murphy
+ */
+public interface UnsupportedListener {
+
+	/**
+	 * Called when an object is unsupported by the de/serializer.
+	 * @param argException the exception describing the error
+	 * @return if the process should continue
+	 */
+	public boolean isUnsupported(UnsupportedObjectException argException);
+}
