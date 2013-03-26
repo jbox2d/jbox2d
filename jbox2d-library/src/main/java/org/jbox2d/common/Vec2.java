f4e3446237c3b47c3f297af851b41b0ff9bc202f
Michael Bayne <mdb@samskivert.com>
2013-03-26 20:43:47 +0000

    Omit @Override from clone(); GWT (unfortunately) lacks Object.clone().

--- jbox2d-library/src/main/java/org/jbox2d/common/Vec2.java
+++ jbox2d-library/src/main/java/org/jbox2d/common/Vec2.java
@@ -172,7 +172,7 @@ public class Vec2 implements Serializable {
     y = MathUtils.abs(y);
   }
 
-  @Override
+  // @Override // annotation omitted for GWT-compatibility
   /** Return a copy of this vector. */
   public final Vec2 clone() {
     return new Vec2(x, y);
