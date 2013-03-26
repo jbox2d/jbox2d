30040ea3d0c4e267b58f2774f14cf64ed1803929
Michael Bayne <mdb@samskivert.com>
2013-03-26 20:55:03 +0000

    Use abstract factory method instead of reflection to create contents.
    This allows the code to be GWT compatible (GWT doesn't support reflection). This patch also
    eliminates the use of typed arrays internal to the stacks and uses Object[] instead. The
    performance benefit of typed arrays versus Object[] is neglible or possibly even negative in this
    case.

--- trunk/jbox2d-library/src/main/java/org/jbox2d/pooling/normal/OrderedStack.java
+++ trunk/jbox2d-library/src/main/java/org/jbox2d/pooling/normal/OrderedStack.java
@@ -26,35 +26,29 @@
  */
 package org.jbox2d.pooling.normal;
 
-import java.lang.reflect.Array;
-
 /**
  * @author Daniel Murphy
  */
-public class OrderedStack<E> {
-  private final E[] pool;
+public abstract class OrderedStack<E> {
+
+  private final Object[] pool;
   private int index;
   private final int size;
-  private final E[] container;
+  private final Object[] container;
 
-  @SuppressWarnings("unchecked")
-  public OrderedStack(Class<E> argClass, int argStackSize, int argContainerSize) {
+  public OrderedStack(int argStackSize, int argContainerSize) {
     size = argStackSize;
-    pool = (E[]) Array.newInstance(argClass, argStackSize);
+    pool = new Object[argStackSize];
     for (int i = 0; i < argStackSize; i++) {
-      try {
-        pool[i] = argClass.newInstance();
-      } catch (Exception e) {
-        throw new RuntimeException("Error creating pooled object " + argClass.getSimpleName(), e);
-      }
+      pool[i] = newInstance();
     }
     index = 0;
-    container = (E[]) Array.newInstance(argClass, argContainerSize);
+    container = new Object[argContainerSize];
   }
 
   public final E pop() {
     assert (index < size) : "End of stack reached, there is probably a leak somewhere";
-    return pool[index++];
+    return (E) pool[index++];
   }
 
   public final E[] pop(int argNum) {
@@ -62,11 +56,14 @@ public class OrderedStack<E> {
     assert (argNum <= container.length) : "Container array is too small";
     System.arraycopy(pool, index, container, 0, argNum);
     index += argNum;
-    return container;
+    return (E[]) container;
   }
 
   public final void push(int argNum) {
     index -= argNum;
     assert (index >= 0) : "Beginning of stack reached, push/pops are unmatched";
   }
+
+  /** Creates a new instance of the object contained by this stack. */
+  protected abstract E newInstance();
 }
