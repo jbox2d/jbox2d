080cccf6c11f9a7f6b318a085a39d7bf8c5522fb
Daniel Murphy <danielmurphy161@gmail.com>
2014-01-05 00:49:42 -0800

    Updated git ignore and classpath

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ jbox2d
 ======
 
 a 2d Java physics engine, native port of the C++ Box2d engine
+
+
+If you've downloaded this as an archive, you should find the built java jars in the 'target' directories of each project.
+
+jbox2d-library - this is the main physics library.  The only dependency is the SLF4J logging library.
+
+jbox2d-serialization - this adds serialization tools.  Requires google's protocol buffer library installed to fully build (http://code.google.com/p/protobuf/), but this is optional, as the generated sources are included.
+
+jbox2d-testbed - A simple framework for creating and running physics tests.
--- jbox2d-library/src/main/java/org/jbox2d/profile/SettingsPerformanceTest.java
+++ jbox2d-library/src/main/java/org/jbox2d/profile/SettingsPerformanceTest.java
@@ -4,7 +4,7 @@ import org.jbox2d.common.Settings;
 
 public abstract class SettingsPerformanceTest extends BasicPerformanceTest {
 
-  private static int NUM_TESTS = 8;
+  private static int NUM_TESTS = 14;
 
   public SettingsPerformanceTest(int iters) {
     super(NUM_TESTS, iters);
@@ -26,6 +26,15 @@ public abstract class SettingsPerformanceTest extends BasicPerformanceTest {
       Settings.FAST_FLOOR = true;
       Settings.FAST_ROUND = true;
       Settings.SINCOS_LUT_ENABLED = true;
+    }
+
+    if (testNum > 7) {
+      Settings.FAST_ABS = testNum != 8;
+      Settings.FAST_ATAN2 = testNum != 9;
+      Settings.FAST_CEIL = testNum != 10;
+      Settings.FAST_FLOOR = testNum != 11;
+      Settings.FAST_ROUND = testNum != 12;
+      Settings.SINCOS_LUT_ENABLED = testNum != 13;
     }
 
     runBenchmarkWorld();
@@ -52,6 +61,18 @@ public abstract class SettingsPerformanceTest extends BasicPerformanceTest {
         return "Sincos lookup table";
       case 7:
         return "All optimizations on";
+      case 8:
+        return "no Fast abs";
+      case 9:
+        return "no Fast atan2";
+      case 10:
+        return "no Fast ceil";
+      case 11:
+        return "no Fast floor";
+      case 12:
+        return "no Fast round";
+      case 13:
+        return "no Sincos lookup table";
       default:
         return "";
     }
