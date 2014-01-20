ccd6513c4c0903dd909e881984d4540419338905
Daniel Murphy <danielmurphy161@gmail.com>
2014-01-19 21:52:16 -0800

    Added particle types, updated pooling a bit, voronoi diagram, and stack queue

---
+++ jbox2d-library/src/main/java/org/jbox2d/particle/ParticleColor.java
@@ -0,0 +1,36 @@
+package org.jbox2d.particle;
+
+import org.jbox2d.common.Color3f;
+
+/**
+ * Small color object for each particle
+ * 
+ * @author dmurph
+ */
+public class ParticleColor {
+  public byte r, g, b, a;
+
+  public ParticleColor() {}
+
+  public ParticleColor(byte r, byte g, byte b, byte a) {
+    set(r, g, b, a);
+  }
+
+  public ParticleColor(Color3f color) {
+    set(color);
+  }
+
+  public void set(Color3f color) {
+    r = (byte) (255 * color.x);
+    g = (byte) (255 * color.y);
+    b = (byte) (255 * color.z);
+    a = (byte) 255;
+  }
+
+  public void set(byte r, byte g, byte b, byte a) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+  }
+}
