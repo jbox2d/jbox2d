060ec40af22a80fa66e5f4063e18a65ab3657036
Daniel Murphy <danielmurphy161@gmail.com>
2014-01-19 22:19:41 -0800

    Switched from enum to int constants for types, added group def

--- jbox2d-library/src/main/java/org/jbox2d/particle/ParticleType.java
+++ jbox2d-library/src/main/java/org/jbox2d/particle/ParticleType.java
@@ -5,30 +5,24 @@ package org.jbox2d.particle;
  * 
  * @author dmurph
  */
-public enum ParticleType {
-  WATER(0),
+public class ParticleType {
+  public static final int b2_waterParticle = 0;
   /** removed after next step */
-  ZOMBIE(1 << 1),
+  public static final int b2_zombieParticle = 1 << 1;
   /** zero velocity */
-  WALL(1 << 2),
+  public static final int b2_wallParticle = 1 << 2;
   /** with restitution from stretching */
-  SPRING(1 << 3),
+  public static final int b2_springParticle = 1 << 3;
   /** with restitution from deformation */
-  ELASTIC(1 << 4),
+  public static final int b2_elasticParticle = 1 << 4;
   /** with viscosity */
-  VISCOUS(1 << 5),
+  public static final int b2_viscousParticle = 1 << 5;
   /** without isotropic pressure */
-  POWDER(1 << 6),
+  public static final int b2_powderParticle = 1 << 6;
   /** with surface tension */
-  TENSILE(1 << 7),
+  public static final int b2_tensileParticle = 1 << 7;
   /** mixing color between contacting particles */
-  COLOR_MIXING(1 << 8),
+  public static final int b2_colorMixingParticle = 1 << 8;
   /** call b2DestructionListener on destruction */
-  DESTRUCTION_LISTENER(1 << 9);
-
-  public final int flag;
-
-  private ParticleType(int flag) {
-    this.flag = flag;
-  }
+  public static final int b2_destructionListener = 1 << 9;
 }
