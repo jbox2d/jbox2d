7c7bb711acd9ead8a01a46a45ec6b9267d1122ee
Jamie Hewland <jhewland@gmail.com>
2014-12-09 23:57:28 +0200

    Remove unused operations in ToI
    * See: https://code.google.com/p/box2d/source/detail?r=250&path=/trunk/Box2D/Box2D/Collision/b2TimeOfImpact.cpp

--- jbox2d-library/src/main/java/org/jbox2d/collision/TimeOfImpact.java
+++ jbox2d-library/src/main/java/org/jbox2d/collision/TimeOfImpact.java
@@ -490,10 +490,6 @@ class SeparationFunction {
 
     switch (m_type) {
       case POINTS: {
-        Rot.mulTransUnsafe(xfa.q, m_axis, axisA);
-        Rot.mulTransUnsafe(xfb.q, m_axis.negateLocal(), axisB);
-        m_axis.negateLocal();
-
         localPointA.set(m_proxyA.getVertex(indexA));
         localPointB.set(m_proxyB.getVertex(indexB));
 
@@ -507,9 +503,6 @@ class SeparationFunction {
         Rot.mulToOutUnsafe(xfa.q, m_axis, normal);
         Transform.mulToOutUnsafe(xfa, m_localPoint, pointA);
 
-        Rot.mulTransUnsafe(xfb.q, normal.negateLocal(), axisB);
-        normal.negateLocal();
-
         localPointB.set(m_proxyB.getVertex(indexB));
         Transform.mulToOutUnsafe(xfb, localPointB, pointB);
         float separation = Vec2.dot(pointB.subLocal(pointA), normal);
@@ -518,9 +511,6 @@ class SeparationFunction {
       case FACE_B: {
         Rot.mulToOutUnsafe(xfb.q, m_axis, normal);
         Transform.mulToOutUnsafe(xfb, m_localPoint, pointB);
-
-        Rot.mulTransUnsafe(xfa.q, normal.negateLocal(), axisA);
-        normal.negateLocal();
 
         localPointA.set(m_proxyA.getVertex(indexA));
         Transform.mulToOutUnsafe(xfa, localPointA, pointA);
